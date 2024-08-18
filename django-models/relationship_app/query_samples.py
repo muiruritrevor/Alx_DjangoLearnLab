8a0793496689bee5b18babc5089974f59e5ad007
muiruritrevor <trevormuirurii@gmail.com>
2024-08-18 23:40:58 +0300

    commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,6 +1,7 @@
 from relationship_app.models import Author, Book, Library,  Librarian
 
 def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
     books = Book.objects.filter(author_name=author_name)
     for book in books:
         print(f"Title: (book.title), Author: {book.author.name}")
