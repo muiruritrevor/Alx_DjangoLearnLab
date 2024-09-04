5fa0e801ba0d68e0d38865c0800e92bf824252ce
muiruritrevor <trevormuirurii@gmail.com>
2024-09-04 23:39:32 +0300

    Defined url patterns

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import AuthorListCreateAPIView, BookListCreateAPIView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('authors/', AuthorListCreateAPIView.as_view(), name='author-list-create'),
-    path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
