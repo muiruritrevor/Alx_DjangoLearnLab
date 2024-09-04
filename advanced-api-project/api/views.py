984e68797917c8c4d809436bb5fc12964863dcaa
muiruritrevor <trevormuirurii@gmail.com>
2024-09-04 23:39:50 +0300

    commit changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,44 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Author, Book
 from .seriealizers import AuthorSerializer, BookSerializer
 
-class AuthorListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can create books
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can update books
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can delete books
+
+
+
+
+
+
+# class AuthorListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorSerializer
+
+# class BookListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
 # Create your views here.
