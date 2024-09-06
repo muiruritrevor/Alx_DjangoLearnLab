f832d4db6005c23abdc145f46779f67a1e58acac
muiruritrevor <trevormuirurii@gmail.com>
2024-09-06 18:29:55 +0300

    commit changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework
 from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticated
 from .models import Author, Book
