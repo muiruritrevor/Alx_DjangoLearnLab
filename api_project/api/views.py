80d53a0e242726e35d638a9c69acbf15215c1037
muiruritrevor <trevormuirurii@gmail.com>
2024-09-09 06:22:04 +0300

    commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
 
