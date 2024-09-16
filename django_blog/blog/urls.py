085e5b732d58f7eabe27a71683ad3b8043f29678
muiruritrevor <trevormuirurii@gmail.com>
2024-09-16 23:05:24 +0300

    commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     #path('posts/', views.posts, name='posts'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-def home(request):
-    return render(request, 'blog/home.html')
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            messages.success(request, 'Registration successful.')
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
-# def posts(request):
-#     return render(request, 'blog/posts.html')
-# # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+class CustomLogoutView(LogoutView):
+    next_page = 'home'
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully.')
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
